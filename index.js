9bf433e8602cd827d16d58d7fb5d55b5db05e28e
pa5anFernando <116500296+pa5anFernando@users.noreply.github.com>
2023-04-19 12:10:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+document.getElementById("Hoist").innerHTML = "<MWSL><!-- write(GetVar('Operation_hours\\Operation_hours.Hoist_hours')); --></MWSL>"
+
+document.getElementById("Trolly").innerHTML = "<MWSL><!-- write(GetVar('Operation_hours\\Operation_hours.Trolly_hours')); --></MWSL>"
+
+document.getElementById("Gantry").innerHTML = "<MWSL><!-- write(GetVar('Operation_hours\\Operation_hours.Gantry_hours')); --></MWSL>"
+
+document.getElementById("Boom").innerHTML = "<MWSL><!-- write(GetVar('Operation_hours\\Operation_hours.Boom_hours')); --></MWSL>"
+
+document.getElementById("Control").innerHTML = "<MWSL><!-- write(GetVar('Operation_hours\\Operation_hours.Control_ON_hours')); --></MWSL>"
+
+*/
+
+//document.getElementById("Wind").innerHTML = "<MWSL><!-- write(GetVar('默认变量表\\默认变量表.WINDSPEED')); --></MWSL>"
